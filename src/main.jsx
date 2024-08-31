a74eb4cee338c0381aa49afc0e1300e194f6934c
Anabela Guillermo <anabela.guillermo@gmail.com>
2024-08-30 23:18:18 -0300

    Creo rutas y header base

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { RouterProvider } from "react-router-dom";
+import { router } from "./constants/routes.jsx";
+import { Toaster } from "sonner";
+
+import "./index.css";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.min.js";
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+      <Toaster richColors position='top-center' />
+    </QueryClientProvider>
+  </React.StrictMode>
+);
