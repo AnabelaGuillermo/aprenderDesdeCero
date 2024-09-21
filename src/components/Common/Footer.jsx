34cc9522a719810d3f898e7b0134394e273ea13e
Anabela Guillermo <anabela.guillermo@gmail.com>
2024-09-21 20:50:26 -0300

    Agrego footer con su estilo

--- src/components/Common/Footer.jsx
+++ src/components/Common/Footer.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
+import "./Footer.css";
+
 const Footer = () => {
-  return <div className="text-center">Footer</div>;
+  return (
+    <section className="footerArcoiris text-center text-white">
+      <h3 className="m-2">Gracias por usar Aprender de Cero</h3>
+      <p>Esperamos te sea de utilidad</p>
+    </section>
+  );
 };
 
 export default Footer;
