0648657418bc7301b7cbd508dbe8af6675516af7
Anabela Guillermo <anabela.guillermo@gmail.com>
2024-09-30 03:03:41 -0300

    Modifico detalles css

--- src/components/Common/Footer.jsx
+++ src/components/Common/Footer.jsx
@@ -5,7 +5,7 @@ import "./Footer.css";
 const Footer = () => {
   return (
     <section className="footerArcoiris text-center text-white">
-      <h4 className="m-2">Gracias por usar Aprender de Cero</h4>
+      <h4 className="mt-3">Gracias por usar Aprender de Cero</h4>
       <p>Esperamos te sea de utilidad</p>
     </section>
   );
