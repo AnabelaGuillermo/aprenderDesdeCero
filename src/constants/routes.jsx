d0c5323590b05e0cb16390fb2bd6aa2f6ce3d4e2
Anabela Guillermo <anabela.guillermo@gmail.com>
2024-11-02 01:25:34 -0300

    Añado colores y lectura en voz alta al hacer clic.

--- src/constants/routes.jsx
+++ src/constants/routes.jsx
@@ -1,11 +1,12 @@
 import { createBrowserRouter } from "react-router-dom";
 import RootView from "../views/routing/RootView";
-import HomeView from "../views/HomeView"
+import HomeView from "../views/HomeView";
 import AlphabetView from "../views/AlphabetView";
-import WriteAndListenView from "../views/WriteAndListenView"
+import WriteAndListenView from "../views/WriteAndListenView";
 import ReadView from "../views/ReadView";
 import Error404View from "../views/Error404View";
-import NumbersView from "../views/NumbersView"
+import NumbersView from "../views/NumbersView";
+import ColorsView from "../views/ColorsView";
 
 export const router = createBrowserRouter([
   {
@@ -31,6 +32,10 @@ export const router = createBrowserRouter([
       {
         path: "Números",
         element: <NumbersView />,
+      },
+      {
+        path: "Colores",
+        element: <ColorsView />,
       },
       {
         path: "*",
