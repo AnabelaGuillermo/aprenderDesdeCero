70b6d0d756da4e7768dc775ae99704f0c73a2324
Anabela Guillermo <anabela.guillermo@gmail.com>
2024-11-03 16:44:13 -0300

    Agrego dropdown, itemCard y componente principal.

--- src/views/HomeStuffView.jsx
+++ src/views/HomeStuffView.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from 'react';
+import Dropdown from '../components/HomeStuffView/Dropdown';
+import { data } from '../data';
 
-const HomeStuffView = () => {
+function App() {
   return (
-    <div>HomeStuffView</div>
-  )
+    <section className="container">
+      <h1>Cosas de la casa</h1>
+      <Dropdown title="Cocina" items={data.Cocina} />
+      <Dropdown title="Living Comedor" items={data.LivingComedor} />
+      <Dropdown title="Cuarto" items={data.Cuarto} />
+      <Dropdown title="Baño" items={data.Baño} />
+      <Dropdown title="Lavaro" items={data.Lavadero} />
+      <Dropdown title="Patio" items={data.Patio} />
+    </section>
+  );
 }
 
-export default HomeStuffView
+export default App;
