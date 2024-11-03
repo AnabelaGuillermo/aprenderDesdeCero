720562db1caccab0c4519c638d999869bcfc0e82
Anabela Guillermo <anabela.guillermo@gmail.com>
2024-11-03 18:15:34 -0300

    Agrego estilos a los botones.

--- src/components/HomeView/ColorButtons.jsx
+++ src/components/HomeView/ColorButtons.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+import '../../css/ColorButtons.css'
+
 const ColorButtons = () => {
   return (
-    <article className="color-buttons">
+    <article className="colorButtonsHome">
       <Link to="/Abecedario" className="btn red-btn">Abecedario</Link>
       <Link to="/Escribe y escucha" className="btn blue-btn">Escribe y escucha</Link>
       <Link to="/Lectura en voz alta" className="btn green-btn">Lectura en voz alta</Link>
-      <Link to="/Números" className="btn yellow-btn">Números</Link>
+      <Link to="/Números" className="btn lightblue-btn">Números</Link>
       <Link to="/Colores" className="btn purple-btn">Colores</Link>
       <Link to="/Cosas de la casa" className="btn orange-btn">Cosas de la casa</Link>
     </article>
