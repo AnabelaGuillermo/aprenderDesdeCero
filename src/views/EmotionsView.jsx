f5aaa386ad81cb1d28c989fb854044612a09bb38
Anabela Guillermo <anabela.guillermo@gmail.com>
2024-11-30 23:35:48 -0300

    Añado emociones.

--- src/views/EmotionsView.jsx
+++ src/views/EmotionsView.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from "react";
+import ItemCard from "../components/HomeStuffView/ItemCard";
+import ColorButtons from "../components/HomeView/ColorButtons";
+import { emotionsView } from "../data/emotions.js";
 
-const EmotionsView = () => {
+import "../css/VegetablesView.css";
+
+function EmotionsView() {
   return (
-    <div>EmotionsView</div>
-  )
+    <section className="container mt-5 text-center">
+      <h1>Emociones</h1>
+      <p className="mb-5">Haz clic en una emoción para escuchar su nombre.</p>
+      <article className="vegetables-content">
+        {emotionsView.Emociones.map((emociones, index) => (
+          <ItemCard key={index} item={emociones} />
+        ))}
+      </article>
+      <article className="mt-5 mb-2">
+        <hr />
+        <ColorButtons />
+      </article>
+    </section>
+  );
 }
 
-export default EmotionsView
+export default EmotionsView;
