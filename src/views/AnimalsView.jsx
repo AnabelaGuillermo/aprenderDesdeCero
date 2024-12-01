1f7c4280aee6889065796df2eab64d7aa31370b3
Anabela Guillermo <anabela.guillermo@gmail.com>
2024-11-30 21:36:27 -0300

    Añado mapeo y cards de animales.

--- src/views/AnimalsView.jsx
+++ src/views/AnimalsView.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from "react";
+import ItemCard from "../components/HomeStuffView/ItemCard";
+import ColorButtons from "../components/HomeView/ColorButtons";
+import { animalsView } from "../data/animals.js";
 
-const AnimalsView = () => {
+import "../css/VegetablesView.css";
+
+function AnimalsView() {
   return (
-    <div>AnimalsView</div>
-  )
+    <section className="container mt-5 text-center">
+      <h1>Animales</h1>
+      <p className="mb-5">Haz clic en un animal para escuchar su nombre.</p>
+      <article className="vegetables-content">
+        {animalsView.Animales.map((animales, index) => (
+          <ItemCard key={index} item={animales} />
+        ))}
+      </article>
+      <article className="mt-5 mb-2">
+        <hr />
+        <ColorButtons />
+      </article>
+    </section>
+  );
 }
 
-export default AnimalsView
+export default AnimalsView;
